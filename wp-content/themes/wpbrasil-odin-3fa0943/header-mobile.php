cb3338c45f36d184b349889c1479b69f4a3f077c
jfsimon-pro <77901095+jfsimon-pro@users.noreply.github.com>
2021-07-19 11:52:13 -0300

    mobile home

--- wp-content/themes/wpbrasil-odin-3fa0943/header-mobile.php
+++ wp-content/themes/wpbrasil-odin-3fa0943/header-mobile.php
@@ -18,11 +18,28 @@
 		<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
 	<?php endif; ?>
 	<?php wp_head(); ?>
+
+
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
+
+	
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/assets-custom/reset.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/assets-custom/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-slider-m.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-searchbar-m.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-lowerbar-m.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-exames-m.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-content-blog-m.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-menu-m.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/mobile/css/home/style-depoimento-m.css">
 </head>
 
 <body <?php body_class(); ?>>
 	
-
-	<div id="wrapper" class="container">
-		<div class="row">
-		<h1>mobile hein lkkkk</h1>
