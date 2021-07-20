4522169ebec6824ac0f61c9a50a9addbd95b1337
jfsimon-pro <77901095+jfsimon-pro@users.noreply.github.com>
2021-07-20 16:11:14 -0300

    examesingle

---
+++ wp-content/themes/wpbrasil-odin-3fa0943/assets/mobile/single-exame-m.php
@@ -0,0 +1,234 @@
+
+    <section class="imgazul">
+        <div class="container-fluid container-eixo">  
+            <div class="objeto-imgazul">
+                            
+            </div>
+        </div>
+    </section>
+    <section class="tab-info-exames">
+        <div class="conteudo-info-exames conteudo-box-info">
+            <div class="box-titulo-cta">
+                <h1>Ecografia Cervical</h1>
+                <a href="#"><button>Agende seu exame!</button></a>
+            </div>
+            <div class="panel with-nav-tabs panel-primary">
+                <div class="panel-heading">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1primary" data-toggle="tab">Onde fazer</a></li>
+                            <li><a href="#tab2primary" data-toggle="tab">Orientações</a></li>
+                            <li><a href="#tab3primary" data-toggle="tab">Conheça mais</a></li>                        
+                        </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content conteudo-tab-unica-exame-info">
+                        <div class="tab-pane fade in active" id="tab1primary">
+                            <div class="tab-parte-unidades">
+                                <h2>Unidades</h2>
+                                <div class="listaunidades-tab-exames">
+                                    <div class="card-unidade-exame-lista">
+                                        <div class="pin-unidade-exame">
+                                            <img src="#">
+                                        </div>
+                                        <div class="text-unidade-exame">
+                                            <h3>Centro Clínico Norte II</h3>
+                                            <p>SCLN 116 Bloco H loja 33, Edifício Buriti – Térreo
+                                                Brasília- DF – CEP: 70710-100
+                                            </p>
+                                            <div class="link-tab-unidade-exames">
+                                                <a href="#">Ver no mapa</a>
+                                                <a href="#">Ver detalhes</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-unidade-exame-lista">
+                                        <div class="pin-unidade-exame">
+                                            <img src="#">
+                                        </div>
+                                        <div class="text-unidade-exame">
+                                            <h3>Centro Clínico Norte II</h3>
+                                            <p>SCLN 116 Bloco H loja 33, Edifício Buriti – Térreo
+                                                Brasília- DF – CEP: 70710-100
+                                            </p>
+                                            <div class="link-tab-unidade-exames">
+                                                <a href="#">Ver no mapa</a>
+                                                <a href="#">Ver detalhes</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-unidade-exame-lista">
+                                        <div class="pin-unidade-exame">
+                                            <img src="#">
+                                        </div>
+                                        <div class="text-unidade-exame">
+                                            <h3>Centro Clínico Norte II</h3>
+                                            <p>SCLN 116 Bloco H loja 33, Edifício Buriti – Térreo
+                                                Brasília- DF – CEP: 70710-100
+                                            </p>
+                                            <div class="link-tab-unidade-exames">
+                                                <a href="#">Ver no mapa</a>
+                                                <a href="#">Ver detalhes</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="outras-informacoes">
+                                <div class="cobertura-convenios">
+                                    <h2>
+                                        Cobertura de convênios
+                                    </h2>
+                                    <p>
+                                        Para informações sobre cobertura de convênio,
+                                        consulte nossa página de Convênios ou entre
+                                        em contato com a nossa Central de Atendimento
+                                        pelo <span>Whatsapp:</span> <a href="#">(61) 99976-4074</a>.
+                                    </p>
+                                </div>
+
+                                <div class="preco-eco">
+                                    <h2>
+                                        Preço da Ecografia no DF
+                                    </h2>
+                                    <p>
+                                        Para saber mais detalhes sobre o preço/valor do
+                                        exame de Ecografia no DF com o IMEB, assim
+                                        como formas de pagamento e outras informações,<a href="#">
+                                        faça o pré-agendamento do exame em uma das
+                                        unidades do Imeb clicando aqui.</a> Nós iremos entrar
+                                        em contato e esclarecer as suas dúvidas mais
+                                        pontuais sobre o seu exame.
+                                    </p>
+                                    <div class="sociais-exame">
+                                        <img src="#">
+                                        <img src="#">
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
+
+
+
+
+
+
+
+
+
+                        </div>
+                        <div class="tab-pane fade" id="tab2primary">
+                            <div class="texto-orientacoes">
+                                <h2>PREPARO</h2>
+                                <ul>
+                                    <li class="turno-orientacao">Manhã</li>
+                                    <li>- Jejum de 06hs para todas as unidades.</li>
+                                </ul>
+                                <ul>
+                                    <li class="turno-orientacao">Tarde</li>
+                                    <li>- Jejum de 06hs para todas as unidades.</li>
+                                    <li>- 30 gotas de SIMETICONA (LUFTAL ou FLAGASS) 2hs antes.</li>
+                                </ul>
+                                <h2>VANTAGENS</h2>
+                                <ul>
+                                    <li>-Não já desconforto ou risco ao paciente.</li>
+                                    <li>-Grande acessibilidade a diversos órgãos.</li>
+                                </ul>
+                            </div>
+                        </div>                        
+                        <div class="tab-pane fade" id="tab3primary">
+                            <div class="primeira-conheca">
+                                <h2>TIPOS DE ECOGRAFIA</h2>
+                                <ul>
+                                    <li>-Ecografia <a href="#">da bolsa escrotal</a></li>
+                                    <li>-Ecografia <a href="#">da fossa clavicular</a></li>
+                                    <li>-Ecografia <a href="#">da paratireóide </a></li>
+                                    <li>-Ecografia <a href="#">da parede abdominal</a></li>
+                                    <li>-Ecografia <a href="#">da Parótida</a></li>
+                                    <li>-Ecografia <a href="#">da próstata via abdominal</a></li>
+                                    <li>-Ecografia <a href="#">da próstata via transretal</a></li>
+                                    <li>-Ecografia <a href="#">da região inguinal</a></li>
+                                    <li>-Ecografia <a href="#">da vesícula</a></li>
+                                    <li>-Ecografia <a href="#">das axilas</a></li>
+                                    <li>-Ecografia <a href="#">das glândulas salivares</a></li>
+                                    <li>-Ecografia <a href="#">das mamas</a></li>
+                                    <li>-Ecografia <a href="#">das mamas 3D</a></li>
+                                    <li>-Ecografia <a href="#">das mãos</a></li>
+                                    <li>-Ecografia <a href="#">das panturrilhas</a></li>
+                                    <li>-Ecografia <a href="#">das pernas</a></li>
+                                    <li>-Ecografia <a href="#">das vias biliares</a></li>
+                                    <li>-Ecografia <a href="#">do abdômen superior</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="segunda-conheca">
+                                <h2>OUTROS NOME</h2>
+                                <ul>
+                                    <li>-Ecografia</li>
+                                    <li>-Ultrassonografia</li>
+                                    <li>-Ultrasom</li>
+                                </ul>
+                            </div>
+
+                            <div class="terceira-conheca">
+                                <h2>O QUE É?</h2>
+                                <p>
+                                    A <a href="#">ecografia ou ultrassonografia</a> é um meio
+                                    complementar de diagnóstico que utiliza as
+                                    propriedades dos ultrasons, nomeadamente
+                                    a sua reflexão nas estruturas do organismo, já
+                                    que certas estruturas refletem mais ou menos
+                                    os sons, enquanto outras (por exemplo a água)
+                                    os deixam passar. Estes exames servem para
+                                    auxiliar o médico no diagnóstico de várias
+                                    doenças, assim como para avaliar a resposta
+                                    a diversos tratamentos.
+                                </p>
+                            </div>
+                            <div class="quarta-conheca">
+                                <h2>VÍDEO E CONTEÚDOS</h2>
+                                <div class="vide-unico-exames">
+                                    <iframe width="350" height="215" src="https://www.youtube.com/embed/O8oNlmspFjQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    </section>
+    <section class="barra-fixa">
+        <div class="container-fluid container-eixo lowerbar">
+            <div class="lower-bar">
+                <div class="quadrante">
+                    <a href="#"></a>
+                    <img src="./assets/imagens/barra-fixa-mobile/resultado.png" class="img-fluid lower-img-eixo">
+                    </a>
+                </div>
+                <div class="quadrante">
+                    <a href="#"></a>
+                    <img src="./assets/imagens/barra-fixa-mobile/agenda.png" class="img-fluid lower-img-eixo">
+                    </a>
+                </div>
+                <div class="quadrante">
+                    <a href="#"></a>
+                    <img src="./assets/imagens/barra-fixa-mobile/azulogo.png" class="img-fluid lower-img-eixo alogoazul">
+                    </a>
+                </div>
+                <div class="quadrante">
+                    <a href="#"></a>
+                    <img src="./assets/imagens/barra-fixa-mobile/whatsapp.png" class="img-fluid lower-img-eixo">
+                    </a>
+                </div>
+                <div class="quadrante">
+                    <a href="#"></a>
+                    <img src="./assets/imagens/barra-fixa-mobile/fone.png" class="img-fluid lower-img-eixo">
+                    </a>
+                </div>
+            </div>            
+        </div>
+    </section>
